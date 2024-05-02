08a2fabae046c60fa10f95073f5a8dddac4422ec
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-03 02:27:36 +0300

    Initial commit

--- src/week2/Q06.java
+++ src/week2/Q06.java
@@ -2,8 +2,8 @@ package week2;
 //A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget. Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, return -1.
 //Example
 //6 = 60
-//keyboards = (40, 50, 60]
-//drives = 5,8, 12]
+//keyboards = [40, 50, 60]
+//drives = [5,8, 12]
 //The person can buy a 40 keyboard + 12 USB drive = 52, or a
 //50 keyboard + 8 USB drive = 58. Choose the latter as the more expensive option
 //and return 58.
