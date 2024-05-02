99c753840b86cc7992b28634065e9122eef6a81e
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-03 02:04:51 +0300

    Initial commit

--- src/week2/Q01.java
+++ src/week2/Q01.java
@@ -1,6 +1,17 @@
 package week2;
+import java.util.Scanner;
 
 // Write a java code that finds whether the number received from the user is odd or even and prints it to the screen.
 public class Q01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int number = scanner.nextInt();
 
+        if (number % 2 == 0){
+            System.out.println("The number that you entered " + number + " is even number." );
+        }else {
+            System.out.println("The number that you entered" + number + " is odd number." );
+        }
+    }
 }
--- src/week2/Q02.java
+++ src/week2/Q02.java
@@ -1,5 +1,24 @@
 package week2;
+import java.util.Scanner;
 
+//Kullanıcıdan alınan sayının faktöriyelini bulan ve ekrana yazdıran java kodu yazınız
 // Write a java code that finds the factorial of the number received from the user and prints it on the screen.
 public class Q02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number that you want to find the factorial of it: ");
+        int number = scanner.nextInt();
+        int sum=1;
+        if (number < 0){
+            System.out.println("The negative numbers doesn't has a factorial value. ");
+        }
+
+        for(int i = 2 ; i <= number ; ++i){
+            sum *= i;
+        }
+        System.out.println("the factorial is:  " + sum);
+
+
+
+    }
 }
