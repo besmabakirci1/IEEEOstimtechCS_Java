4bff7558fa6075b1e0001c099d57023a41a59767
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-02 17:34:17 +0300

    Initial commit

--- src/week1/Q05.java
+++ src/week1/Q05.java
@@ -1,12 +1,28 @@
 package week1;
+import java.util.Scanner;
 
 // Write the algorithm of the program that finds which of the two entered numbers is smaller and prints the result to the screen.
 // Step 1: Start
 // Step 2: Enter the first number(number1)
 // Step 3: Enter second number(number2)
 // Step 4: If number1 == number2, "number and argument are the same."
-// Step 5: If number1 > integer, "the number is less." is displayed on the screen.
-// Step 6: If the number is < number, "number is less" is displayed on the screen.
+// Step 5: If number1 > number2, "the number is less." is displayed on the screen.
+// Step 6: If the number1 < number2, "number is less" is displayed on the screen.
 // Step 7: Finish
 public class Q05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the first number(number1): ");
+        double number1 = scanner.nextDouble();
+        System.out.println("Please enter the second number(number2): ");
+        double number2 = scanner.nextDouble();
+        if (number1 == number2){
+            System.out.println("Number and argument are the same");
+        } else if (number1 >number2) {
+            System.out.println("The number1" + number1 + "is more than number2" + number2);
+        }else {
+            System.out.println("The number1" + number1 + "is less than number2" + number2);
+        }
+
+    }
 }
--- src/week1/Q06.java
+++ src/week1/Q06.java
@@ -1,14 +1,28 @@
 package week1;
-//Algorithm and flowchart that calculates the sum of integers from 1 to N
+
+import java.util.Scanner;
+
+// Algorithm and flowchart that calculates the sum of integers from 1 to N
 // Step 1: Start
 // Step 2: Enter the number N(N)
-// Step 3: assign o to the total variable. (total=o)
-// Step 4: Assign o to the counter variable. (counter=o)
-// Step 5: If counter>N-1, go to Step8.
+// Step 3: assign 0 to the total variable. (total=o)
+// Step 4: Assign 0 to the counter variable. (counter=o)
+// Step 5: If counter > N-1, go to Step8.
 // Step 6: increase the combat value by 1. (savac = counter+1)
 // Step 7: add the total variable and the counter variable and get the result
 //assign it to the total variable and go to Step5. (total = total+ counter)
 // Step 8: write the total variable to the screen.
 // Step 9: Finish
 public class Q06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        double N = scanner.nextDouble();
+        double total = 0;
+        double counter = 0;
+        if (counter > N-1){
+            System.out.println(total);
+        }
+
+    }
 }
