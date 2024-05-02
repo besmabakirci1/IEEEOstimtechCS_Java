b3b4893c1a1e92a272f58f5377590ea5d77ba99d
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-03 00:55:51 +0300

    Initial commit

--- src/week1/Q06.java
+++ src/week1/Q06.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 // Step 3: assign 0 to the total variable. (total=o)
 // Step 4: Assign 0 to the counter variable. (counter=o)
 // Step 5: If counter > N-1, go to Step8.
-// Step 6: increase the combat value by 1. (savac = counter+1)
+// Step 6: increase the combat value by 1. (counter = counter+1)
 // Step 7: add the total variable and the counter variable and get the result
 //assign it to the total variable and go to Step5. (total = total+ counter)
 // Step 8: write the total variable to the screen.
@@ -20,9 +20,22 @@ public class Q06 {
         double N = scanner.nextDouble();
         double total = 0;
         double counter = 0;
-        if (counter > N-1){
-            System.out.println(total);
-        }
+        while(true){
+            if (counter > (N-1)){
+                System.out.println(total);
+            } else {
+                counter = counter + 1;
+                total = total + counter;
+            }
 
+
+
+            while (counter < N) {
+                counter = counter + 1;
+                total = total + counter;
+            }
+
+            System.out.println("The sum of integers from 1 to " + N + " is: " + total);
+        }
     }
 }
